1ed85aeb193bcbaeb3c386e90a065c728b392c16
Andrew Reid <areid.sj@gmail.com>
2020-08-27 11:38:36 -0600

    Refactor method

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,12 +1,8 @@
 class Acronym
   class << self
     def abbreviate(string)
-      output = ''
       split_string = string.split(/[\s,-]/)
-      split_string.each do |word|
-        output += word[0].upcase if word =~ /[[:alpha:]]/
-      end
-      output
+      split_string.map { |word| word[0].upcase if word =~ /[[:alpha:]]/}.join
     end
   end
 end
