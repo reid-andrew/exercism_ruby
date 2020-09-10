9b6ee3a9e592dc29c04fc0508e6197abd8832caf
Andrew Reid <areid.sj@gmail.com>
2020-09-10 09:34:23 -0600

    Refactor for readability

--- series/series.rb
+++ series/series.rb
@@ -1,23 +1,11 @@
 class Series
   def initialize(numbers)
-    @numbers = numbers.split(//)
+    @numbers = numbers.chars
   end
 
   def slices(value)
     raise ArgumentError.new if value > @numbers.size
 
-    output = @numbers.each_with_index.map do |number, index|
-      build_string(index, value)
-    end
-    output.select { |number| !number.nil? }
-  end
-
-  def build_string(index, value)
-    string = ""
-    value.times do
-      string += @numbers[index] if @numbers[index]
-      index += 1
-    end
-    string if string.size == value
+    @numbers.each_cons(value).map(&:join)
   end
 end
