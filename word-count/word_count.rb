5104ae8b87cd3a8e6cfb7957e52a5ef1ae2c8526
Andrew Reid <areid.sj@gmail.com>
2020-09-10 09:55:29 -0600

    Refactor to condense

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,15 +1,11 @@
 class Phrase
   def initialize(phrase)
-    @phrase = phrase.gsub(',', ' ').gsub(/[^0-9a-z ]/i, '')
+    @phrase = phrase.scan(/\w+/)
   end
 
   def word_count
-    words = @phrase.split(' ')
-    words.reduce({}) do |output, word|
-      output[word.downcase] += 1 if output[word.downcase]
-
-      output[word.downcase] = 1 if !output[word.downcase]
-
+    @phrase.reduce(Hash.new(0)) do |output, word|
+      output[word.downcase] += 1
       output
    end
   end
