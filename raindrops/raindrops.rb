00773f52650e5d4166ba2ee8631f464d3a3a01c6
Andrew Reid <areid.sj@gmail.com>
2020-09-10 10:07:33 -0600

    Pass all tests

--- raindrops/raindrops.rb
+++ raindrops/raindrops.rb
@@ -1,8 +1,10 @@
-=begin
-Write your code for the 'Raindrops' exercise in this file. Make the tests in
-`raindrops_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/raindrops` directory.
-=end
-
+class Raindrops
+  def self.convert(value)
+    output = ""
+    output += "Pling" if value % 3 == 0
+    output += "Plang" if value % 5 == 0
+    output += "Plong" if value % 7 == 0
+    output += value.to_s if output == ""
+    output
+  end
+end
