8b2fb4191d5b439307a8fbdc9a81604b869770a0
Andrew Reid <areid.sj@gmail.com>
2020-09-10 09:26:42 -0600

    Pass all series tests

--- series/series.rb
+++ series/series.rb
@@ -1,1 +1,23 @@
+class Series
+  def initialize(numbers)
+    @numbers = numbers.split(//)
+  end
 
+  def slices(value)
+    raise ArgumentError.new if value > @numbers.size
+
+    output = @numbers.each_with_index.map do |number, index|
+      build_string(index, value)
+    end
+    output.select { |number| !number.nil? }
+  end
+
+  def build_string(index, value)
+    string = ""
+    value.times do
+      string += @numbers[index] if @numbers[index]
+      index += 1
+    end
+    string if string.size == value
+  end
+end
