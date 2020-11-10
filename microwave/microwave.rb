b73627b21e95b517d6037a678b650c4d6f22087e
Andrew Reid <areid.sj@gmail.com>
2020-11-10 13:35:19 -0700

    Pass all tests

--- microwave/microwave.rb
+++ microwave/microwave.rb
@@ -1,7 +1,25 @@
-=begin
-Write your code for the 'Microwave' exercise in this file. Make the tests in
-`microwave_test.rb` pass.
+class Microwave
+  def initialize(seconds)
+    @seconds = seconds
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/microwave` directory.
-=end
+  def timer
+    if @seconds < 100
+      minutes = @seconds / 60
+      minute_output = minutes < 10 ? "0#{minutes}" : "#{minutes}"
+      remainder = @seconds - (minutes * 60)
+      remainder_output = remainder < 10 ? "0#{remainder}" : "#{remainder}"
+
+      "#{minute_output}:#{remainder_output}"
+    else
+      sec_string = @seconds.to_s
+      last_two = "#{sec_string[-2]}#{sec_string[-1]}".to_i
+      if last_two > 60
+        @seconds = @seconds - 60 + 100
+        sec_string = @seconds.to_s
+      end
+      first_two = @seconds < 999 ? "0#{sec_string[0]}" : "#{sec_string[0]}#{sec_string[1]}"
+      "#{first_two}:#{sec_string[-2]}#{sec_string[-1]}"
+    end
+  end
+end
